7cdb72a131279a77a52ce21e794cca034155ea8d
albert <63851587@qq.com>
2019-09-03 15:27:41 +0800

    fix docker image and sql

--- sql/seaslog.sql
+++ sql/seaslog.sql
@@ -1,5 +1,25 @@
 create database if not exists logs;
 
+CREATE TABLE if not exists logs.kafka_seaslog
+(
+    `appname`        String,
+    `datetime`       DateTime,
+    `level`          String,
+    `request_uri`    String,
+    `request_method` String,
+    `clientip`       String,
+    `requestid`      String,
+    `filename`       String,
+    `memoryusage`    UInt64,
+    `message`        String
+) ENGINE = Kafka SETTINGS kafka_broker_list = 'kafka:29092',
+    kafka_topic_list = 'seaslog',
+    kafka_group_name = 'clickhouse',
+    kafka_format = 'JSONEachRow',
+    kafka_skip_broken_messages = 1,
+    kafka_num_consumers = 1;
+--消费者数量根据情况自定义
+
 CREATE TABLE if not exists logs.seaslog
 (
     `appname`        String,
@@ -29,24 +49,4 @@ SELECT appname,
        memoryusage,
        message
 FROM logs.kafka_seaslog;
-
-CREATE TABLE if not exists logs.kafka_seaslog
-(
-    `appname`        String,
-    `datetime`       DateTime,
-    `level`          String,
-    `request_uri`    String,
-    `request_method` String,
-    `clientip`       String,
-    `requestid`      String,
-    `filename`       String,
-    `memoryusage`    UInt64,
-    `message`        String
-) ENGINE = Kafka SETTINGS kafka_broker_list = 'kafka:29092',
-    kafka_topic_list = 'seaslog',
-    kafka_group_name = 'clickhouse',
-    kafka_format = 'JSONEachRow',
-    kafka_skip_broken_messages = 1,
-    kafka_num_consumers = 1;
---消费者数量根据情况自定义
 
