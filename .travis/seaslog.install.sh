cb7cc629e10d76059b4aad2f59fbed6907169bdc
wujunze <itwujunze@163.com>
2019-09-09 14:42:17 +0800

    rename Seaslog file name

--- .travis/seaslog.install.sh
+++ .travis/seaslog.install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-wget https://github.com/SeasX/SeasLog/archive/${SL_VERSION}.tar.gz
+wget https://github.com/SeasX/SeasLog/archive/${SL_VERSION}.tar.gz -O SeasLog.tar.gz
 mkdir -p SeasLog
 tar -xf SeasLog.tar.gz -C SeasLog --strip-components=1
 rm SeasLog.tar.gz
